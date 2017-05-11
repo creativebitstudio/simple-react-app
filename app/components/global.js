775119253605bafe394bb5e4d604a340aef4ecc5
Danielson <creativebitstudio@gmail.com>
2017-05-11 15:29:00 -0400

    react files added

---
+++ app/components/global.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import logo from './public/react-logo.png';
+
+class global extends Component{
+    render(){
+        return(
+            <div>
+            <div className="container">
+                <img src={'https://vincent.billey.me/talks/react-putting-js-in-your-face/assets/react-logo.png'} alt="logo"/>
+                <h2>Welcome to another Creativebitstudio Codebit Sample</h2>
+                <hr/>
+                <h3>This is a Simple React App, Enjoy!</h3>
+            </div>
+            </div>
+        )
+    }
+} 
+
+export default global;
